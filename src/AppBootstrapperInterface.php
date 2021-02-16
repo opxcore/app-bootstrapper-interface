db683cdc01651d012bc9d7c1cfae6448934d0671
lozovoyv <lozovoyv@gmail.com>
2021-02-16 11:52:51 +0300

    Add instance key

--- src/AppBootstrapperInterface.php
+++ src/AppBootstrapperInterface.php
@@ -12,5 +12,19 @@ namespace OpxCore\App\Interfaces;
 
 interface AppBootstrapperInterface
 {
+    /**
+     * This function will be called on bootstrapping.
+     *
+     * @param AppInterface $app
+     *
+     * @return  void
+     */
     public function bootstrap(AppInterface $app): void;
+
+    /**
+     * Get key to be used to instance in container. If null returned no instancing needed.
+     *
+     * @return  string|null
+     */
+    public function getKeyForInstance(): ?string;
 }
